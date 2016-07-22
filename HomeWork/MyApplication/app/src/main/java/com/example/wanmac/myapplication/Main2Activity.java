45f2704601c511ae822b0c92d727e0ba9a777f4d
wanMac <wanaaaa@gmail.com>
2016-07-22 15:52:47 -0400

    asdf

--- HomeWork/MyApplication/app/src/main/java/com/example/wanmac/myapplication/Main2Activity.java
+++ HomeWork/MyApplication/app/src/main/java/com/example/wanmac/myapplication/Main2Activity.java
@@ -3,6 +3,9 @@ package com.example.wanmac.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -19,29 +22,29 @@ public class Main2Activity extends AppCompatActivity {
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.main_menu, menu);
-//
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item){
-//        switch (item.getItemId()){
-//            case R.id.iconOne:
-//                //startActivity(new Intent(this, Main2Activity.class));
-//                Toast.makeText(Main2Activity.this, "One clicked", Toast.LENGTH_SHORT).show();
-//                return true;
-//            case R.id.iconTwo:
-//                Toast.makeText(Main2Activity.this, "Two Clicked", Toast.LENGTH_SHORT).show();
-//                return true;
-//            case R.id.icoThree:
-//                Toast.makeText(Main2Activity.this, "Three Clicked", Toast.LENGTH_SHORT).show();
-//                return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.iconOne:
+                //startActivity(new Intent(this, Main2Activity.class));
+                Toast.makeText(Main2Activity.this, "One clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.iconTwo:
+                Toast.makeText(Main2Activity.this, "Two Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.icoThree:
+                Toast.makeText(Main2Activity.this, "Three Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
